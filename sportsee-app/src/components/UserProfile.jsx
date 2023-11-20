2b12c851861141c9b5a287bb5c2a5612f260924a
khanh.truong <abby79truong@gmail.com>
2023-11-20 15:54:22 +0100

    step 10: user Story #13, 14: RadarChart

--- sportsee-app/src/components/UserProfile.jsx
+++ sportsee-app/src/components/UserProfile.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import UserHeader from "./UserHeader";
 import UserBarChart from "./UserBarChart";
 import UserAverageSessionDuration from "./UserAverageSessionDuration";
+import UserRadarChart from "./UserRadarChart";
 import "../styles/UserProfile.css";
 import { useParams } from "react-router-dom";
 
@@ -13,7 +14,7 @@ const UserProfile = () => {
       <UserHeader userId={id} />
       <UserBarChart userId={id} />
       <UserAverageSessionDuration userId={id} />
-      
+      <UserRadarChart userId={id} />    
     </div>
   );
 };
---
+++ sportsee-app/src/components/UserRadarChart.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  RadarChart,
+  Radar,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+} from "recharts";
+import "../styles/UserRadarChart.css";
+import { mockUserPerformance } from "../services/mockData";
+
+const UserRadarChart = ({ userId }) => {
+  const userPerformanceData = mockUserPerformance.find(
+    (data) => data.userId === parseInt(userId)
+  );
+
+  if (!userPerformanceData) {
+    console.error(`User performance data not found for ID: ${userId}`);
+    return <p className="error-message">User performance data not found</p>;
+  }
+
+  // Custom tick formatter
+  const formatTick = (value) => {
+    return userPerformanceData.kind[value + 1];
+  };
+
+  return (
+    <div className="user-radar-chart">
+      <RadarChart
+        className="polygons"
+        outerRadius={90}
+        width={180}
+        height={180}
+        data={userPerformanceData.data}
+      >
+        <PolarGrid />
+        <PolarAngleAxis tickFormatter={formatTick} />
+        <PolarRadiusAxis angle={30} />
+        <Radar
+          name="User"
+          dataKey="value"
+          stroke="white"
+          fill="#FF0101"
+          fillOpacity={0.7}
+        />
+      </RadarChart>
+    </div>
+  );
+};
+
+export default UserRadarChart;
